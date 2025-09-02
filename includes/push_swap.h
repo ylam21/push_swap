4779297400f9dbdd278e97040ce135e95cfd53ba
ylam21 <ohnivasasanka@gmail.com>
2025-09-02 12:12:05 +0200

    minor

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: omaly <omaly@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/01 16:00:24 by omaly             #+#    #+#             */
-/*   Updated: 2025/09/02 11:50:11 by omaly            ###   ########.fr       */
+/*   Updated: 2025/09/02 11:55:45 by omaly            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,11 +30,9 @@ typedef struct s_ps
 	int				size_a;
 	int				highest_a;
 }					t_ps;
-// Core
+// Main
 void				push_swap(t_ps *ps);
 void				init_ps(t_ps *ps, int argc, char **argv);
-// temporary
-void				print_stack(t_list *stack);
 // Operations
 void				push(t_list **src, t_list **dest);
 void				rotate(t_list **stack);
@@ -42,13 +40,13 @@ void				reverse_rotate(t_list **stack);
 // Parsing and validation
 t_list				*parse(int argc, char **argv);
 int					validate_args(int argc, char **argv);
-// List utilities
+// Debug
+void				print_stack(t_list *stack);
+// Utilities for operations on t_list
 t_list				*ft_lstnew(int d);
 t_list				*ft_lstlast(t_list *lst);
 void				ft_lstadd_back(t_list **lst, t_list *newlst);
 int					ft_lstsize(t_list *lst);
-// Other push_swap utils
-int					ft_atoi(const char *s);
 void				free_stack(t_list *stack);
 int					is_ascending(t_list *lst);
 int					is_descending(t_list *lst);
@@ -56,6 +54,8 @@ int					get_index(t_list *lst, int find);
 int					get_max(t_list *stack);
 int					get_min(t_list *stack);
 int					get_target_index(t_list **stack_a, int size_a);
+// Other Utilities
+int					ft_atoi(const char *s);
 int					get_closer_index(int index_a, int index_b, int size);
 int					distance_to_top(int size, int index);
 int					is_digit(int c);
