268056f8bd224fefc966abfaf790a9ed74369234
Ondrej <ohnivasasanka@gmail.com>
2025-10-01 13:36:39 +0200

    easier readability

--- src/push_swap.c
+++ src/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: omaly <omaly@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/20 10:33:46 by omaly             #+#    #+#             */
-/*   Updated: 2025/10/01 13:21:04 by omaly            ###   ########.fr       */
+/*   Updated: 2025/10/01 13:36:10 by omaly            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,19 @@ int	main(int argc, char **argv)
 	t_ps	ps;
 
 	if (argc < 2)
+	{
 		return (1);
+	}
 	if (validate_args(argc, argv) != 0)
-		return (print_error(2), 1);
+	{
+		print_error(2);
+		return (2);
+	}
 	if (init_ps(&ps, argc, argv) != 0)
-		return (print_error(2), 1);
+	{
+		print_error(2);
+		return (3);
+	}
 	push_swap(&ps);
 	free_ps(&ps);
 	return (0);
